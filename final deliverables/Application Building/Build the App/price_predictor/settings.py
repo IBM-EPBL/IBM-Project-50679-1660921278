1f5fee23c20dcc2fe9fc2b1f1b5a53eb1df7b834
K.Pranitha <55057295+pranithakuppusamy@users.noreply.github.com>
2022-11-15 23:59:40 +0530

    Update settings.py

--- final deliverables/Application Building/Build the App/price_predictor/settings.py
+++ final deliverables/Application Building/Build the App/price_predictor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '@poi-z61l0_&%37^%paz#(u4azz9i42ze0#f4g5ry-7%s**lw$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','carpricepredictor2020.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','carpricepredictor2022.herokuapp.com']
 
 
 # Application definition
